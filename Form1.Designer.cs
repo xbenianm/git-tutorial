92ab45007fcedfa21ce5f1a8def2e4d58283f0e3
Matúš Benian <123726707+xbenianm@users.noreply.github.com>
2025-02-22 10:35:08 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ZOZNAMULOH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NADPIS = new System.Windows.Forms.TextBox();
+            this.POPIS = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(330, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 104);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ZOZNAM ULOH";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // NADPIS
+            // 
+            this.NADPIS.Location = new System.Drawing.Point(12, 75);
+            this.NADPIS.Name = "NADPIS";
+            this.NADPIS.Size = new System.Drawing.Size(1061, 22);
+            this.NADPIS.TabIndex = 1;
+            // 
+            // POPIS
+            // 
+            this.POPIS.Location = new System.Drawing.Point(12, 136);
+            this.POPIS.Name = "POPIS";
+            this.POPIS.Size = new System.Drawing.Size(1061, 22);
+            this.POPIS.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NADPIS:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "POPIS:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 55);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "NOVY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(277, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 55);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "UPRAV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(552, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(243, 55);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "VYMAZ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(830, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(243, 55);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "ULOZ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 225);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1058, 337);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1093, 584);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.POPIS);
+            this.Controls.Add(this.NADPIS);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ZoznamUloh";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NADPIS;
+        private System.Windows.Forms.TextBox POPIS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZOZNAMULOH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable toDolist = new DataTable();
+        bool isEditing = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //VYTVOR TABULKU Z HODNOTAMI
+            toDolist.Columns.Add("NADPIS");
+            toDolist.Columns.Add("POPIS");
+            //PREPOJENIE TABULKY S PREMENNOU
+            dataGridView1.DataSource = toDolist;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //NAPIS VSETKY UDAJE
+            NADPIS.Text = "";
+            POPIS.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isEditing = true;
+            //VYPLNI DATA
+            NADPIS.Text = toDolist.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[0].ToString();
+            POPIS.Text = toDolist.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[1].ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                toDolist.Rows[dataGridView1.CurrentCell.RowIndex].Delete();
+        }
+            catch (Exception ex )
+            {
+                Console.WriteLine("Chyba:" + ex);
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if ( isEditing ) {
+                toDolist.Rows[dataGridView1.CurrentCell.RowIndex]["NADPIS"] = NADPIS.Text;
+                toDolist.Rows[dataGridView1.CurrentCell.RowIndex]["POPIS"] = POPIS.Text;
+            }
+            else
+            {
+                toDolist.Rows.Add(NADPIS.Text, POPIS.Text);
+                NADPIS.Text = "";
+                POPIS.Text = "";
+                isEditing = false;
+            }
+        }
+}
+}
